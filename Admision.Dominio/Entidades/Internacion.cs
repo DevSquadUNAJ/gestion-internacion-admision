e8a07c4bfcc4c88eb54a6d1a45974190574f0483
Godoy-Rodrigo <godoyrodrigo859@gmail.com>
2026-06-05 20:21:08 -0300

    feat(dominio): crear entidades POCO de Admision con propiedades de navegacion

---
+++ Admision.Dominio/Entidades/Internacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Admision.Dominio.Entidades
+{
+    public class Internacion
+    {
+        public Guid Id { get; set; }
+        public Guid PacienteId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime? FechaEgreso { get; set; }
+        public string Motivo { get; set; }
+        public string Estado { get; set; }
+        public virtual Paciente Paciente { get; set; }
+        public virtual ICollection<InternacionCama> HistorialCamas { get; set; } = new List<InternacionCama>();
+    }
+}
---
+++ Admision.Dominio/Entidades/InternacionCama.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Admision.Dominio.Entidades
+{
+    public class InternacionCama
+    {
+        public Guid Id { get; set; }
+        public Guid InternacionId { get; set; }
+        public Guid CamaId { get; set; }
+        public DateTime FechaIngresoCama { get; set; }
+        public DateTime? FechaSalidaCama { get; set; }
+        public bool EsActual { get; set; }
+
+        public virtual Internacion Internacion { get; set; }
+        public virtual Cama Cama { get; set; }
+    }
+}
