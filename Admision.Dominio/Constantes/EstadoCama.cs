5e8d9210d1dbc479581fcf41e6ba7240d544c896
Godoy-Rodrigo <godoyrodrigo859@gmail.com>
2026-06-05 20:39:09 -0300

    feat(dominio): agregar constantes estaticas para estados de cama e internacion

---
+++ Admision.Dominio/Constantes/EstadoCama.cs
@@ -0,0 +1,10 @@
+﻿namespace Admision.Dominio.Constantes
+{
+    public static class EstadoCama
+    {
+        public const string Disponible = "Disponible";
+        public const string Ocupada = "Ocupada";
+        public const string Mantenimiento = "Mantenimiento";
+        public const string Limpieza = "Limpieza";
+    }
+}
